a73bf8c304624797877a411db334a12f20262810
Paul Swartz <paulswartz@gmail.com>
2012-04-27 12:47:43 -0400

    add a util function to rescale a video

--- mvc/utils.py
+++ mvc/utils.py
@@ -2,3 +2,37 @@ def hms_to_seconds(hours, minutes, seconds):
     return (hours * 3600 +
             minutes * 60 +
             seconds)
+
+
+def round_even(num):
+    """This takes a number, converts it to an integer, then makes
+    sure it's even.
+
+    Additional rules: this helper always rounds down to avoid stray black
+    pixels (see bz18122).
+
+    This function makes sure that the value returned is always >= 0.
+    """
+    num = int(num)
+    val = num - (num % 2)
+    return val if val > 0 else 0
+
+
+def rescale_video((source_width, source_height),
+                  (target_width, target_height)):
+    """
+    Rescale a video given a (width, height) target.  This returns the largest
+    (width, height) which maintains the original aspect ratio while fitting
+    within the target size.
+    """
+    if not source_width or not source_height:
+        return (target_width, target_height)
+
+    if (source_width <= target_width and
+        source_height <= target_height):
+        return (source_width, source_height)
+
+    width_ratio = float(source_width) / float(target_width)
+    height_ratio = float(source_height) / float(target_height)
+    ratio = max(width_ratio, height_ratio)
+    return round_even(source_width / ratio), round_even(source_height / ratio)
--- test/test_utils.py
+++ test/test_utils.py
@@ -15,3 +15,22 @@ class UtilsTest(base.Test):
                          3 * 3600 +
                          2 * 60 +
                          1.5)
+
+    def test_round_even(self):
+        self.assertEqual(utils.round_even(-1), 0)
+        self.assertEqual(utils.round_even(0), 0)
+        self.assertEqual(utils.round_even(1), 0)
+        self.assertEqual(utils.round_even(2), 2)
+        self.assertEqual(utils.round_even(2.5), 2)
+        self.assertEqual(utils.round_even(3), 2)
+
+    def test_rescale_video(self):
+        target = (1024, 768)
+        self.assertEqual(utils.rescale_video(target, target),
+                         target)
+        self.assertEqual(utils.rescale_video((512, 384), target), # small
+                         (512, 384))
+        self.assertEqual(utils.rescale_video((2048, 1536), target), # big
+                         target)
+        self.assertEqual(utils.rescale_video((1400, 768), target), # widescreen
+                         (1024, 560))
