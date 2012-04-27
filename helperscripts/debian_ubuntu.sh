6bbb2f1b0a9c3918afe332176631c8a6f3188ead
Paul Swartz <paulswartz@gmail.com>
2012-04-26 23:43:07 -0400

    add helperscripts for recent debian/ubuntu

---
+++ helperscripts/debian_ubuntu.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# This script installs dependencies for building and running MVC on
+# Debian Wheezy (currently testing).
+#
+# You run this sript AT YOUR OWN RISK.  Read through the whole thing
+# before running it!
+#
+# This script must be run with sudo.
+
+# Last updated:    2012-04-26
+# Last updated by: Paul Swartz
+
+apt-get install \
+    python-gtk2 \
+    ffmpeg \
+    ffmpeg2theora
