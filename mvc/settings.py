af048c41576c2b92eea722858f10027ce88fc4fb
Paul Swartz <paulswartz@gmail.com>
2012-04-30 16:04:14 -0400

    find ffmpeg/ffmpeg2theora, don't hardcode the path

--- mvc/settings.py
+++ mvc/settings.py
@@ -1,5 +1,24 @@
-def get_ffmpeg_executable_path():
-    return "/usr/bin/ffmpeg"
+import os
 
+def which(name):
+    for dirname in os.environ['PATH'].split(os.pathsep):
+        fullpath = os.path.join(dirname, name)
+        # XXX check for +x bit
+        if os.path.exists(fullpath):
+            return fullpath
+
+def memoize(func):
+    cache = []
+    def wrapper():
+        if not cache:
+            cache.append(func())
+        return cache[0]
+    return wrapper
+
+@memoize
+def get_ffmpeg_executable_path():
+    return which("ffmpeg")
+
+@memoize
 def get_ffmpeg2theora_executable_path():
-    return "/usr/bin/ffmpeg2theora"
+    return which("ffmpeg2theora")
