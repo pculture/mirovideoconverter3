7636d53944ea974a25e5913ed9e4697cb4cd1d0b
Geoffrey Lee <glee@pculture.org>
2012-06-05 23:10:58 +0900

    sandbox build scripts.

---
+++ build.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+~/src/miro/miro/sandbox_20120516/Frameworks/Python.framework/Versions/2.7/bin/python setup.py develop
+
+~/src/miro/miro/sandbox_20120516/Frameworks/Python.framework/Versions/2.7/bin/python setup.py py2app
+
+codesign -fs '3rd Party Mac Developer Application: Participatory Culture Foundation' --entitlements sandbox.entitlements dist/widgets.app
