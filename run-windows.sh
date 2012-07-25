fe3a72a0984a23da8f7dbd0a2560ab3da4c8ca66
Ben Dean-Kawamura <ben@pculture.org>
2012-07-25 20:54:31 +0100

    Added script to run MVC on windows

---
+++ run-windows.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+if [ ! -e mvc-env ] ; then
+        echo "MVC virtualenv is not present.  Run "
+        echo
+        echo "   python helperscripts/windows-virtualenv/ mvc-env"
+        echo
+        echo "to build it"
+        exit 1
+fi
+
+PYTHONPATH="." mvc-env/Scripts/python.exe mvc
