61b9962823a15ea15a008c5c9ddb5fdeccca71e5
Geoffrey Lee <glee@pculture.org>
2012-09-09 13:02:58 +0900

    Fixups for Python.framework: py2app doesn't copy complete Python framework.

--- sign.sh
+++ sign.sh
@@ -7,6 +7,14 @@ do
       --entitlements ffmpeg.entitlements "${i}"
 done
 
+# Fix up the python framework installation from py2app: it doesn't
+# copy over all the files that constitutes it to be a valid framework.
+pushd "dist/Miro Video Converter.app/Contents/Frameworks/Python.framework"
+ln -sf Versions/Current/Python Python
+ln -sf Versions/Current/Resources Resources
+ln -sf 2.7 Versions/Current
+popd
+
 codesign -fs \
   '3rd Party Mac Developer Application: Participatory Culture Foundation' \
   --entitlements ffmpeg.entitlements \
