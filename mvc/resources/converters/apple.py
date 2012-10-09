9ffb51eacd60bc8e9f24c67a21dda6d4b635d7d2
Ben Dean-Kawamura <ben@pculture.org>
2012-10-09 23:14:35 +0000

    bz19608: added more converters

--- mvc/resources/converters/apple.py
+++ mvc/resources/converters/apple.py
@@ -18,10 +18,11 @@ ipod_touch = AppleConversion('iPod Touch', 640, 480)
 ipod_retina = AppleConversion('iPod Touch 4+', 960, 640)
 iphone = AppleConversion('iPhone', 640, 480)
 iphone_retina = AppleConversion('iPhone 4+', 960, 640)
+iphone_5 = AppleConversion('iPhone 5', 1920, 1080)
 ipad = AppleConversion('iPad', 1024, 768)
 ipad_retina = AppleConversion('iPad 3', 1920, 1080)
 apple_tv = AppleConversion('Apple TV', 1280, 720)
 universal = AppleConversion('Apple Universal', 1280, 720)
 
-converters = [ipod, ipod_touch, ipod_retina, iphone, iphone_retina, ipad,
-              ipad_retina, apple_tv, universal]
+converters = [ipod, ipod_touch, ipod_retina, iphone, iphone_retina, iphone_5,
+	ipad, ipad_retina, apple_tv, universal]
