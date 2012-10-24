b23c109131b9fa7711e133af200a3f57e22d2fb1
Ben Dean-Kawamura <ben@pculture.org>
2012-10-24 12:22:50 -0400

    PSP format fixes (#19660)
    Changed PSP format to be 320x240.  Removed the -s parameter from the
    paramaters list, we add that ourselves now.

--- mvc/resources/converters/others.py
+++ mvc/resources/converters/others.py
@@ -3,7 +3,7 @@ from mvc.converter import FFmpegConverterInfo
 class PlaystationPortable(FFmpegConverterInfo):
     media_type = 'other'
     extension = 'mp4'
-    parameters = ('-s 320x240 -b 512000 -ar 24000 -ab 64000 '
+    parameters = ('-b 512000 -ar 24000 -ab 64000 '
                   '-f psp -r 29.97').split()
 
 
@@ -14,7 +14,7 @@ class KindleFire(FFmpegConverterInfo):
                   '-preset slow -f mp4 -crf 22').split()
 
 
-psp = PlaystationPortable('Playstation Portable', 480, 320)
+psp = PlaystationPortable('Playstation Portable', 320, 240)
 kindle_fire = KindleFire('Kindle Fire', 1224, 600)
 
 converters = [psp, kindle_fire]
